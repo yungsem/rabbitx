37c55bfa05b2f3665e43834e5effc48b19a40352
yungsem <yungsem@126.com>
2022-12-04 14:11:15 +0800

    first commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/yungsem/rabbitx
+
+go 1.19
+
+require github.com/rabbitmq/amqp091-go v1.5.0
---
+++ rabbitx.go
@@ -0,0 +1,145 @@
+package rabbitx
+
+import (
+	"fmt"
+	amqp "github.com/rabbitmq/amqp091-go"
+	"log"
+)
+
+// Rabbitx 用于封装对 RabbitMQ 的操作
+type Rabbitx struct {
+	conn *amqp.Connection
+}
+
+// New 创建一个 Rabbitx 实例
+func New(username string, password string, host string, port string) (*Rabbitx, error) {
+	// 构建连接地址：amqp://username:password@host:port/
+	uriFmt := "amqp://%s:%s@%s:%s/"
+	uri := fmt.Sprintf(uriFmt, username, password, host, port)
+
+	// 建立连接
+	conn, err := amqp.Dial(uri)
+	if err != nil {
+		return nil, err
+	}
+
+	return &Rabbitx{conn: conn}, nil
+}
+
+// Channel 获取 channel
+func (r *Rabbitx) Channel() (*amqp.Channel, error) {
+	channel, err := r.conn.Channel()
+	if err != nil {
+		return nil, err
+	}
+	return channel, nil
+}
+
+// Consume 消费队列里的消息
+func (r *Rabbitx) Consume(queueName string, autoAck bool, handleFunc func(amqp.Delivery)) error {
+	channel, err := r.Channel()
+	if err != nil {
+		return err
+	}
+	defer channel.Close()
+
+	deliveries, err := channel.Consume(
+		queueName,
+		"consumer-of-"+queueName,
+		autoAck,
+		false,
+		false,
+		false,
+		nil,
+	)
+	if err != nil {
+		return err
+	}
+
+	// 打印消息体
+	for delivery := range deliveries {
+		// 消息处理，handleFunc 无需负责 ack
+		handleFunc(delivery)
+		if autoAck == false {
+			errAck := delivery.Ack(false)
+			if errAck != nil {
+				// TODO: 这里的处理方式存疑
+				log.Printf("ack error: %s\n", err)
+			}
+		}
+	}
+
+	return nil
+}
+
+// DeclareExchange 声明 exchange
+func (r *Rabbitx) DeclareExchange(exchangeName string, exchangeType string) error {
+	// 获取 channel
+	channel, err := r.Channel()
+	if err != nil {
+		return err
+	}
+	defer channel.Close()
+
+	// 声明 exchange
+	err = channel.ExchangeDeclare(
+		exchangeName,
+		exchangeType,
+		true,
+		false,
+		false,
+		false,
+		nil,
+	)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// DeclareQueue 声明 queue
+func (r *Rabbitx) DeclareQueue(queueName string) (*amqp.Queue, error) {
+	// 获取 channel
+	channel, err := r.Channel()
+	if err != nil {
+		return nil, err
+	}
+	defer channel.Close()
+
+	// 声明 queue
+	queue, err := channel.QueueDeclare(
+		queueName,
+		true,
+		false,
+		false,
+		false,
+		nil,
+	)
+	if err != nil {
+		return nil, err
+	}
+	return &queue, nil
+}
+
+// Bind 绑定 queue 到 exchange
+func (r *Rabbitx) Bind(queueName string, exchangeName string, routingKey string) error {
+	// 获取 channel
+	channel, err := r.Channel()
+	if err != nil {
+		return err
+	}
+	defer channel.Close()
+
+	// 绑定 exchange 和 queue
+	err = channel.QueueBind(
+		queueName,
+		routingKey,
+		exchangeName,
+		false,
+		nil,
+	)
+	if err != nil {
+		return err
+	}
+	return nil
+}
